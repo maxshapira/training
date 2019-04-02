05a845b80caf1265a46a1d0a00506663a4f1ac8b
maxshapira <46228217+maxshapira@users.noreply.github.com>
2019-04-02 11:20:31 +0300

    Update p.py

--- sub/p.py
+++ sub/p.py
@@ -1,5 +1,5 @@
 text_file = open("/Neteera/Work/max.shapiro/results/Output.txt", "w")
 
-text_file.write("Purchase Amount: " 'TotalAmount')
+text_file.write("Purchase A: " 'TotalAmount')
 
 text_file.close()
